a7579196c3f250416c67af3f6e66aa22fac4b09e
osvaldo <osvaldo.corttex@gmail.com>
2026-05-08 19:17:40 -0300

    Atualiza├º├úo

--- bkpgithub.bat
+++ bkpgithub.bat
@@ -4,6 +4,6 @@ git commit -m "Atualização"
 
 git push
 
-# checar se aparecer nothing to commit, working #tree clean ok fez o backup correto
-
+<# checar se aparecer nothing to commit, working # tree clean ok fez o backup correto
+#>
 git status
